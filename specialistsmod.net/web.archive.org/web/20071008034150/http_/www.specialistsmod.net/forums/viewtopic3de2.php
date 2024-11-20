fc167d625ba310e58c96a5c786eacdf0f4f845cd
2cwldys <sdpayne113@gmail.com>
2024-11-20 02:37:08 -0500

    add specialistsmod.net website

---
+++ specialistsmod.net/web.archive.org/web/20071008034150/http_/www.specialistsmod.net/forums/viewtopic3de2.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from web.archive.org/web/20071008034150/http://www.specialistsmod.net/forums/viewtopic.php?t=21360&sid=c27a5b43b1f703e2597e9ecaf1bc2994 by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 20 Nov 2024 07:24:31 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=https://web.archive.org/web/20070813065847/http://www.specialistsmod.net/forums/viewtopic.php?t=21360%26sid=b31877ff42f091ecae9420e7fa239548"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="https://web.archive.org/web/20070813065847/http://www.specialistsmod.net/forums/viewtopic.php?t=21360%26sid=b31877ff42f091ecae9420e7fa239548"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from web.archive.org/web/20071008034150/http://www.specialistsmod.net/forums/viewtopic.php?t=21360&sid=c27a5b43b1f703e2597e9ecaf1bc2994 by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 20 Nov 2024 07:24:31 GMT -->
+</HTML>
